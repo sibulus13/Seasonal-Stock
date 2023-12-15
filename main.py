978822bb402a0d544ffe6df7bbc795b67db5c351
Sib <chengjie.michael.huang@gmail.com>
2023-12-14 19:28:47 -0800

    Add main function and if __name__ == '__main__': block

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from analysis import create_monthly_dataset
 from alpaca_trade_api.rest import TimeFrame
 
 def main():
+    pass
+
+
+if __name__ == '__main__':
     syms = ['CVE']
-    
+        
     for sym in syms:
         start_date = '2000-01-01'
         fetch_new_data(sym, start_date=start_date, tf=TimeFrame.Week)
